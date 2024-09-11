64d75b5c4069fa6c9b20307b6ff3bfd3ff4c86ed
jeremy.damour <jeremy.damour2@gmail.com>
2024-09-11 14:50:17 +0200

    [Parser] add reference to to filename parser

---
+++ src/interpret.rs
@@ -0,0 +1,36 @@
+use super::args::Args;
+use super::{interpreter, parser, typing};
+use color_print::cformat;
+
+fn debug_print<T>(printing: bool, name: &str, elm: T)
+where
+    T: std::fmt::Display,
+{
+    if printing {
+        let msg = cformat!("<cyan>{name} :</>\n{elm}");
+        eprintln!("{msg}")
+    }
+}
+
+pub fn interpret_file(args: &Args, file_name: String) {
+    let res = Ok(file_name)
+        .and_then(parser::parse_file)
+        .inspect(|parse_tree| debug_print(args.debug_sexp, "SEXP", parse_tree))
+        .and_then(parser::make_program)
+        .inspect(|wt_program| debug_print(args.debug_parser, "Parsed program", wt_program))
+        .and_then(typing::infer_type)
+        .inspect(|t_program| debug_print(args.debug_typer, "Typed program", t_program))
+        .and_then(interpreter::eval_program);
+    match res {
+        Ok(ret_code) => std::process::exit(ret_code.try_into().unwrap()),
+        Err(err) => {
+            let msg = if args.no_color {
+                err.to_string()
+            } else {
+                err.colored()
+            };
+            eprint!("{msg}");
+            std::process::exit(err.get_code())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,21 @@
+mod args;
 mod ast;
 mod error;
+mod interpret;
 mod interpreter;
 mod location;
 mod parser;
+mod repl;
 mod stdlib;
 mod typing;
 
 use clap::Parser;
-use color_print::cformat;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(help = "filename")]
-    file_name: Option<String>,
-
-    #[arg(long, help = "Print sexp tree-sitter parser")]
-    debug_sexp: bool,
-
-    #[arg(long, help = "Print the AST")]
-    debug_parser: bool,
-
-    #[arg(long, help = "Print the typed program")]
-    debug_typer: bool,
-
-    #[arg(long, help = "Don't colorize error")]
-    no_color: bool,
-}
-
-fn debug_print<T>(printing: bool, name: &str, elm: T)
-where
-    T: std::fmt::Display,
-{
-    if printing {
-        let msg = cformat!("<cyan>{name} :</>\n{elm}");
-        eprintln!("{msg}")
-    }
-}
-
-fn eval_file(args: &Args, file_name: String) {
-    let res = Ok(file_name)
-        .and_then(parser::parse_file)
-        .inspect(|parse_tree| debug_print(args.debug_sexp, "SEXP", parse_tree))
-        .and_then(parser::make_program)
-        .inspect(|wt_program| debug_print(args.debug_parser, "Parsed program", wt_program))
-        .and_then(typing::infer_type)
-        .inspect(|t_program| debug_print(args.debug_typer, "Typed program", t_program))
-        .and_then(interpreter::eval_program);
-    match res {
-        Ok(ret_code) => std::process::exit(ret_code.try_into().unwrap()),
-        Err(err) => {
-            let msg = if args.no_color {
-                err.to_string()
-            } else {
-                err.colored()
-            };
-            eprint!("{msg}");
-            std::process::exit(err.get_code())
-        }
-    }
-}
 
 fn main() {
-    let args = Args::parse();
+    let args = args::Args::parse();
 
     match &args.file_name {
-        None => std::process::exit(0),
-        Some(file_name) => eval_file(&args, file_name.to_string()),
+        None => repl::repl(),
+        Some(file_name) => interpret::interpret_file(&args, file_name.to_string()),
     }
 }
