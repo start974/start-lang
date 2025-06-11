2d16e594b9371929cf42bba60ec40656b588e089
jeremy.damour <jeremy.damour2@gmail.com>
2025-06-11 20:46:59 +0200

    [Error] implement error for parser

--- src/parser/ast/program.rs
+++ src/parser/ast/program.rs
@@ -3,11 +3,11 @@ use crate::utils::theme::{Doc, Theme};
 
 use super::{ExpressionDefinition, TyDefinition};
 
-pub enum Item {
+pub enum ProgramItem {
     TyDef(TyDefinition),
     ExprDef(ExpressionDefinition),
 }
-pub struct Program(Vec<Item>);
+pub struct Program(Vec<ProgramItem>);
 
 impl Program {
     /// make an empty program
@@ -15,21 +15,27 @@ impl Program {
         Self(Vec::new())
     }
 
-    /// push an item
-    pub fn push_item(&mut self, item: Item) {
+    /// add an item
+    pub fn add_item(&mut self, item: ProgramItem) {
         self.0.push(item);
     }
 
-    pub fn items(&self) -> &[Item] {
+    /// with item program
+    pub fn with_item(mut self, item: ProgramItem) -> Self {
+        self.add_item(item);
+        self
+    }
+
+    pub fn items(&self) -> &[ProgramItem] {
         &self.0
     }
 }
 
-impl Pretty for Item {
+impl Pretty for ProgramItem {
     fn pretty(&self, theme: &Theme) -> Doc<'_> {
         match self {
-            Item::TyDef(def) => def.pretty(theme),
-            Item::ExprDef(def) => def.pretty(theme),
+            ProgramItem::TyDef(def) => def.pretty(theme),
+            ProgramItem::ExprDef(def) => def.pretty(theme),
         }
     }
 }
