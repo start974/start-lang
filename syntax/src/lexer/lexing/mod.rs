473b082300a23e8a0165b93cdb7546d0be3c6ef5
jeremy.damour <jeremy.damour2@gmail.com>
2026-01-13 18:50:31 +0100

    [Lexer] add module character and make tests

--- syntax/src/lexer/lexing/mod.rs
+++ syntax/src/lexer/lexing/mod.rs
@@ -10,6 +10,7 @@ mod comment;
 mod identifier;
 mod number;
 mod with_meta;
+mod character;
 
 pub use chumsky::prelude::Parser;
 pub use comment::comment;
@@ -17,105 +18,11 @@ pub use identifier::identifier;
 pub use number::{
     digit, digit_bin, digit_hex, digit_oct, number, number_bin, number_dec, number_hex, number_oct,
 };
+use character::character;
 pub use with_meta::WithMeta;
 
 pub type ErrorChumsky<'src> = chumsky::error::Rich<'src, char>;
 pub type ExtraChumsky<'src> = chumsky::extra::Err<ErrorChumsky<'src>>;
-
-// ===========================================================================
-// Character
-// ===========================================================================
-
-/// char with number
-fn escape_number_char<'src>(
-    digit: impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>>,
-    number_digit: usize,
-    radix: u32,
-) -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    let digits = digit.repeated().exactly(number_digit).collect::<String>();
-
-    digits.try_map(move |digits, span| {
-        u8::from_str_radix(&digits, radix)
-            .map(|b| b as char)
-            .map_err(|_| Rich::custom(span, "Invalid escape character"))
-    })
-}
-
-fn escape_number_char_prefixed<'src>(
-    prefix: char,
-    digit: impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>>,
-    number_digit: usize,
-    radix: u32,
-) -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    just(prefix).ignore_then(escape_number_char(digit, number_digit, radix))
-}
-
-fn escape_unicode_char<'src>() -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    just('u')
-        .ignore_then(
-            digit_hex()
-                .repeated()
-                .at_least(1)
-                .collect::<String>()
-                .delimited_by(just('{'), just('}')),
-        )
-        .try_map(|digits, span| {
-            u32::from_str_radix(&digits, 16)
-                .ok()
-                .and_then(std::char::from_u32)
-                .ok_or_else(|| Rich::custom(span, "invalid unicode escape"))
-        })
-}
-
-/// escape char
-///```ebnf
-/// escape_char := "\"
-///    ("\\" | "\"" | "\'" | "n" | "r" | "t"
-///    | digit{3} | "x" digit_hex{2} | "o" digit_oct{3}
-///    | "u{" digit_hex+ "}")
-///```
-fn escape_char<'src>() -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    just('\\').ignore_then(choice((
-        just('\\').to('\\'),
-        just('\"').to('\"'),
-        just('\'').to('\''),
-        just('n').to('\n'),
-        just('r').to('\r'),
-        just('t').to('\t'),
-        escape_number_char(digit(), 3, 10),
-        escape_number_char_prefixed('x', digit_hex(), 2, 16),
-        escape_number_char_prefixed('o', digit_oct(), 3, 8),
-        escape_unicode_char(),
-    )))
-}
-
-/// lex caracter
-/// ```ebnf
-/// character_literal :=
-/// | ESCAPE_CHAR
-/// | [U+0000 .. U+D7FF]
-/// | [U+E000 .. U+10FFFF]
-/// ```
-fn character_lit<'src>() -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    choice((
-        escape_char(),
-        any().filter(|c: &char| {
-            let cp = *c as u32;
-            (cp <= 0xD7FF) || (0xE000..=0x10FFFF).contains(&cp)
-        }),
-    ))
-}
-
-/// lex character
-/// ```ebnf
-/// CHARACTER := "'" CHARACTER_LIT "'"
-/// ```
-pub fn character<'src>() -> impl Parser<'src, &'src str, char, Err<ErrorChumsky<'src>>> {
-    let quote = just('\'').labelled("'");
-    character_lit()
-        .delimited_by(quote, quote)
-        .labelled("character")
-}
 
 // ===========================================================================
 // Keyword
